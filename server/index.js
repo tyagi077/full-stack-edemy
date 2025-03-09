4244ed5c4810a365893a21ebfcc2630071c21e53
Mayank Tyagi <tyagimayank0501@gmail.com>
2025-03-09 08:49:00 +0530

    added vercel.json file

--- server/index.js
+++ server/index.js
@@ -1,37 +1,41 @@
-import dotenv from "dotenv"
-dotenv.config()
-import express from "express"
-import mainRouter from "./routes/index.js"
-import connectDB from "./config/mongodb.js"
-import cors from "cors"
-import connectCloudinary from "./config/cloudinary.js"
-const app = express()
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import mainRouter from "./routes/index.js";
+import connectDB from "./config/mongodb.js";
+import cors from "cors";
+import connectCloudinary from "./config/cloudinary.js";
+
+const app = express();
 
 app.use(cors({
-    origin: ["http://localhost:5173", "https://full-stack-mayank-edemy.vercel.app"], // Allow both local & production frontend
+    origin: ["http://localhost:5173", "https://full-stack-mayank-edemy.vercel.app"],
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    allowedHeaders: "Content-Type, Authorization", // Allow auth headers
-    credentials: true // Allow cookies & credentials
+    allowedHeaders: "Content-Type, Authorization",
+    credentials: true
 }));
 
+const PORT = process.env.PORT || 3000;
+app.use(express.json());
 
-const PORT = process.env.PORT ||3000
+async function startServer() {
+    try {
+        await connectDB();
+        await connectCloudinary();
 
+        app.get("/", (req, res) => {
+            res.send("API is running");
+        });
 
-app.use(express.json())
-await connectDB()
+        app.use("/api/v1/", mainRouter);
 
-app.get("/", (req, res) => {
-    res.send("API is running ");
-});
+        app.listen(PORT, () => {
+            console.log(`Server running on port ${PORT}`);
+        });
+    } catch (error) {
+        console.error("Error starting the server:", error);
+        process.exit(1);
+    }
+}
 
-
-await connectCloudinary()
-
-app.use("/api/v1/",mainRouter)
-
-
-
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+startServer();
