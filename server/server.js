05c8d7e37453531ecf000e940c95af03396e6d4e
Mayank Tyagi <tyagimayank0501@gmail.com>
2025-03-09 09:35:22 +0530

    fix error

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 import app from "./index.js";
 
-const PORT = process.env.PORT || 5000; // Render provides process.env.PORT
+const PORT = process.env.PORT || 10000; // Ensure you use Render's assigned port
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
     console.log(`🚀 Server running on port ${PORT}`);
 });
