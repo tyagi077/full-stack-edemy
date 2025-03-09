66ee1625b15a3ecc8554b8b252a934eecfbf7bc2
Mayank Tyagi <tyagimayank0501@gmail.com>
2025-03-09 09:42:50 +0530

    fix error

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ import connectCloudinary from "./config/cloudinary.js";
 const app = express();
 
 app.use(cors({
-    origin: ["http://localhost:5173", "https://full-stack-mayank-edemy.vercel.app"],
+    origin: ["http://localhost:5173", "https://full-stack-edemy.vercel.app"],
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     allowedHeaders: "Content-Type, Authorization",
     credentials: true
